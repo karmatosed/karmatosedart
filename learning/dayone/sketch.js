4f22cd9cc48f49dd5664a7b53879306bc55786ec
Tammie Lister <tammie@automattic.com>
2021-09-01 21:57:18 +0100

    First commit

---
+++ learning/dayone/sketch.js
@@ -0,0 +1,38 @@
+function setup() {
+  // put setup code here
+  createCanvas(400,400);
+  //strokeWeight(10);
+  //rectMode(CENTER);
+}
+
+function draw() {
+  // put drawing code here
+  background(200);
+  //point(10,10);
+  //point(300,10);
+  //point(10, 300);
+  //point(300, 300);
+
+  //line(0, 0, 400,400); 
+
+  //ellipse(200,200,100);
+
+  //rect(280,280,100,100);
+
+  //arc(200,200,100,100,0,TWO_PI);
+  //arc(200,200,100,100,0,PI/2, PIE);
+
+  arc(200,200,100,100,0,PI/2, CHORD);
+  arc(300,220,100,150,0,PI/2, CHORD);
+  arc(200,270,100,100,0,PI/2, CHORD);
+  arc(200,200,100,100,0,PI/2, CHORD);
+  arc(200,200,150,100,0,PI/2, CHORD);
+  arc(240,200,100,100,0,PI/2, CHORD);
+  arc(200,250,100,100,0,PI/2, CHORD);
+  arc(20,20,10,10,0,PI/2, CHORD);
+  arc(30,30,20,20,0,PI/2, CHORD);
+  arc(25,25,40,40,0,PI/2, CHORD);
+  arc(60,60,60,60,0,PI/2, CHORD);
+  arc(80,80,80,80,0,PI/2, CHORD);
+
+}
